8ea5251e3d78104fb50fadd3077f978f61db85c0
HerikLima <heriklyma@hotmail.com>
2016-02-29 13:17:43 -0300

    Verificar senão houve erro na consulta do CEP.

--- main.cpp
+++ main.cpp
@@ -12,21 +12,28 @@ int main(int argc, char *argv[])
 
     QEventLoop waitLoop;
     QNetworkAccessManager manager;
-    QNetworkReply *response = manager.get(QNetworkRequest(QUrl("http://viacep.com.br/ws/01328010/json/")));
-    QObject::connect(response, SIGNAL(finished()), &waitLoop, SLOT(quit()));
+    QNetworkReply *reply = manager.get(QNetworkRequest(QUrl("http://viacep.com.br/ws/01328010/json")));
+    QObject::connect(reply, SIGNAL(finished()), &waitLoop, SLOT(quit()));
     waitLoop.exec();
 
-    QJsonDocument doc(QJsonDocument::fromJson(response->readAll()));
+    QJsonDocument doc(QJsonDocument::fromJson(reply->readAll()));
     QJsonObject json(doc.object());
 
-    qDebug() << "cep: "           << json["cep"].toString();
-    qDebug() << "logradouro: "    << json["logradouro"].toString();
-    qDebug() << "localidade: "    << json["localidade"].toString();
-    qDebug() << "uf: "            << json["uf"].toString();
-    qDebug() << "complemento: "   << json["complemento"].toString();
-    qDebug() << "bairro: "        << json["bairro"].toString();
-    qDebug() << "ibge: "          << json["ibge"].toString();
-    qDebug() << "gia: "           << json["gia"].toString();
+    if(json.find("erro") != json.end())
+    {
+        qDebug() << "erro: "           << json["erro"].toBool();
+    }
+    else
+    {
+        qDebug() << "cep: "           << json["cep"].toString();
+        qDebug() << "logradouro: "    << json["logradouro"].toString();
+        qDebug() << "localidade: "    << json["localidade"].toString();
+        qDebug() << "uf: "            << json["uf"].toString();
+        qDebug() << "complemento: "   << json["complemento"].toString();
+        qDebug() << "bairro: "        << json["bairro"].toString();
+        qDebug() << "ibge: "          << json["ibge"].toString();
+        qDebug() << "gia: "           << json["gia"].toString();
+    }
 
     return a.exec();
 }
